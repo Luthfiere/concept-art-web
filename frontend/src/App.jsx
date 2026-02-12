ee1d34efc2cfe05c96dd7155452253a8249d43ee
FarhanDPutra <fdanaraksa@gmail.com>
2026-02-12 23:22:10 +0700

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div className="h-screen flex items-center justify-center bg-black">
       <div className="text-center">
         <h1 className="text-5xl font-bold text-yellow-400 mb-4">
-          Tailwind Works 🚀
+          berhasil
         </h1>
         <p className="text-blue-400">
           Kalau background hitam dan tulisan kuning muncul, berarti berhasil.
